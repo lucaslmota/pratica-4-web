a174b9ad8762369ab2f740380ecff44204c40448
lucas <lucaslmota0431@gmail.com>
2021-07-27 10:11:34 -0300

    Views para buscar

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,16 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 
 const routes = [
-  
+  {
+    path: "/",
+    name: "Buscar",
+    component: () => import("../views/Buscar.vue")
+  },
+  {
+    path: "/carros",
+    name:"Carros",
+    component: () => import("../views/Carros.vue")
+  }
 ]
 
 const router = createRouter({
--- src/views/Buscar.vue
+++ src/views/Buscar.vue
@@ -1,13 +1,152 @@
 <template>
-<h1>Buscar</h1>
+ <div class="campos">
+
+          <h4>Mostrar todos os Carros</h4>
+            <button @click="todosCarros()">Todos os carros</button><br /><br />
+                <ul>
+                    <li v-for="carro in todosCarros " :key="carro.id">
+                        <div class="infor">
+                        <p> Id: {{ carro.id }}</p>
+                        <p>Nome: {{ carro.nome }}</p>
+                        <p>Marca: {{ carro.marca }}</p>
+                        <p>Ano de Fabricação: {{ carro.anoFabricacao }}</p>
+                        <p>Ano de Modelo: {{ carro.anoModel }}</p>
+                        <p>Data de Venda: {{ carro.dataVenda }}</p>
+                        </div>
+                    </li>
+                </ul>
+
+        <h4>Filtrar carros por Id</h4>
+        <p>Id: <input type="text" v-model="id"></p>
+          <button @click="BuscarCarroId()">Carros por Id</button><br /><br />
+
+          <ul>
+            <li>
+                <div class="infor">
+                <p> Id: {{ carrosId.id }}</p>
+                <p>Nome: {{ carrosId.nome }}</p>
+                <p>Marca: {{ carrosId.marca }}</p>
+                <p>Ano de Fabricação: {{ carrosId.anoFabricacao }}</p>
+                <p>Ano de Modelo: {{ carrosId.anoModel }}</p>
+                <p>Data de Venda: {{ carrosId.dataVenda }}</p>
+                </div>
+            </li>
+        </ul>
+
+        <h4>Filtrar pela marca do carro</h4>
+             <p> Marca: <input type="text" v-model="marca" /><br /></p>
+             <button @click="buscarCarroMarca()">Buscar carro pela marca</button><br /><br />
+
+                <ul>  
+                    <li  v-for="carro in carroMarca " :key="carro.id" >
+                        <div class="infor">
+                            <p> Id: {{ carro.id }}</p>
+                            <p>Nome: {{ carro.nome }}</p>
+                            <p>Marca: {{ carro.marca }}</p>
+                            <p>Ano de Fabricação: {{ carro.anoFabricacao }}</p>
+                            <p>Ano de Modelo: {{ carro.anoModel }}</p>
+                            <p>Data de Venda: {{ carro.dataVenda }}</p>
+                        </div>
+                    </li>
+                </ul>
+        
+        <h4>Filtrar por quantidade de Carros</h4>
+            Quantidade: <input type="number" v-model="quantidade" /><br /><br /><br>
+             <button @click="buscarCarroQuant()">Buscar por qtd de carros</button><br /><br />
+
+              <ul>
+                    <li  v-for="carro in carroQtd " :key="carro.id" >
+                        <div class="infor">
+                            <p> Id: {{ carro.id }}</p>
+                            <p>Nome: {{ carro.nome }}</p>
+                            <p>Marca: {{ carro.marca }}</p>
+                            <p>Ano de Fabricação: {{ carro.anoFabricacao }}</p>
+                            <p>Ano de Modelo: {{ carro.anoModel }}</p>
+                            <p>Data de Venda: {{ carro.dataVenda }}</p>
+                        </div>
+                    </li>
+               </ul>
+
+        </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
+    data() {
+        return {
+            id: 0,
+            nome: "",
+            marca: "",
+            anoFabricacao:"",
+            anoModel:"",
+            dataVenda:"",
+            quantidade:0,
+            todosCarros: [],
+            carrosId:[],
+            carroMarca:[],
+            carroQtd:[],
+            baseURI:""
+        }
+    },
 
+    methods: {
+        todosCarros(){
+            axios.get(this.baseURI).then((result) =>{
+                this.todosCarros = result.data
+            })
+        },
+
+        BuscarCarroId(){
+            axios.get(this.baseURI + "/" + this.id).then((result) =>{
+                this.carrosId = result.data
+            })
+            .catch(function(error) {
+            console.log(error);
+            })
+        },
+
+        buscarCarroMarca(){
+            axios.get(this.baseURI + "/searchMarca?marca=" + this.marca).then((result) =>{
+                this.carroMarca = result.data
+            })
+            .catch(function(error) {
+            console.log(error);
+            })
+        },
+
+        buscarCarroQuant(){
+            axios.get(this.baseURI + "/searchQTDCarro?quantidade=" + this.carroQtd).then((result) =>{
+                this.carroQtd = result.data
+            })
+            .catch(function(error) {
+            console.log(error);
+            })
+        }
+    },
 }
 </script>
 
 <style>
-
+.Campos {
+    margin: auto;
+    width: 80%;
+        
+}
+button{
+    background-color: rgb(23, 91, 192);
+    color: white;
+    margin: 0 20px;
+    height: 30px;
+    cursor: pointer;
+}
+.infor {
+    display: flex;
+   
+}
+.infor p {
+    margin: 10px 10px;
+    
+    
+}
 </style>
