59018840c4740a0bc764eeb2061f818b0fb6214c
lucas <lucaslmota0431@gmail.com>
2021-07-27 14:41:15 -0300

    Views para cadastrar carros

--- src/views/Carros.vue
+++ src/views/Carros.vue
@@ -1,13 +1,121 @@
 <template>
-  <h1>Carros</h1>
+<div>
+
+
+  <div class="campos">
+    <h4>Cadastrar Carros</h4>
+
+           <p> Id: <input type="text" v-model="id" /><br /></p> 
+           <p> Nome: <input type="text" v-model="nome" /><br /></p> 
+           <p> Marca: <input type="text" v-model="marca" /><br /></p>
+           <p> Ano de Fabricação: <input type="text" v-model="anoFabricacao" /><br /></p>
+           <p> Ano do modelo: <input type="text" v-model="anoModel" /><br /><br></p>
+           <p> Data de Venda: <input type="text" v-model="dataVenda" /><br /><br></p>
+   </div>
+
+  <div class="butaos">
+    <button @click="postCarro()">Post Carro</button><br /><br />
+    <button @click="putCarro()">Put Carro</button><br /><br />
+    <button @click="deleteCarro()">Delete Carro</button><br /><br /> 
+  </div>
+
+   <ul>
+      <li>
+        <div class="infor">
+          <p> Id: {{ carros.id }}</p>
+          <p>Nome: {{ carros.nome }}</p>
+          <p>Marca: {{ carros.marca }}</p>
+          <p>Ano de Fabricação: {{ carros.anoFabricacao }}</p>
+          <p>Ano de Modelo: {{ carros.anoModel }}</p>
+          <p>Data de Venda: {{ carros.dataVenda }}</p>
+        </div>
+      </li>
+    </ul>
+
+
+</div>
 </template>
 
 <script>
+import axios from 'axios';
 export default {
+  data() {
+    return {
+      id: 0,
+      nome: "",
+      marca: "",
+      anoFabricacao:"",
+      anoModel:"",
+      dataVenda:"",
+      carros: [],
+      baseURI: ""
+    }
+  },
+  
+  methods: {
+    postCarro(){
+      let obj ={
+        nome: this.nome,
+        marca: this.marca,
+        anoFabricacao: this.anoFabricacao,
+        anoModel: this.anoModel,
+        dataVenda: this.dataVenda,
+      };
 
+      axios.post(this.baseURI, obj).then((resul) =>{
+        this.carros = resul.data
+      })
+    },
+
+    putCarro(){
+      let obj ={
+        nome: this.nome,
+        marca: this.marca,
+        anoFabricacao: this.anoFabricacao,
+        anoModel: this.anoModel,
+        dataVenda: this.dataVenda,
+      };
+
+      axios.put(this.baseURI, obj).then((result) =>{
+        this.carros = result.data
+      })
+    },
+
+    deleteCarro(){
+      axios.delete(this.baseURI +"/"+this.id).then((result) =>{})
+    }
+  },
 }
 </script>
 
 <style>
+
+.Campos {
+    margin: auto;
+    font-weight: 600;
+    width: 40%;
+        
+}
+.butaos{
+    display: flex;
+    width: 70%;
+    justify-content: center;
+    margin: 10px auto;
+  
+}
+.butaos button{
+    background-color: rgb(23, 91, 192);
+    color: white;
+    margin: 0 20px;
+    cursor: pointer;
+}
+.infor {
+    display: flex;
+}
+.infor p {
+    margin: 10px 10px;
+    
+}
+
 
 </style>
